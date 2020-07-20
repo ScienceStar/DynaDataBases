4612db08a023b307c1890e9956c6e24f7c49f099
sciencestar <1633696870@qq.com>
2020-07-20 17:10:04 +0800

    SpringBoot 三种定时器方式实现

---
+++ src/test/java/com/OptionalTest.java
@@ -0,0 +1,29 @@
+package com;
+
+import com.domin.Person;
+import org.junit.Test;
+
+import java.util.Optional;
+
+/**
+ * @ClassName OptionalTest
+ * @Description: TODO
+ * @Author lxc
+ * @Date 2020/7/20 16:53
+ * @Version V1.0
+ **/
+public class OptionalTest {
+
+    @Test
+    public void testOptional(){
+        Person person = new Person();
+        person.setPersonId(123l);
+        person.setPersonName("mike");
+        Optional.ofNullable(person).ifPresent(p->{
+            System.out.println(p.getPersonName());
+        });
+
+        String personName =Optional.of(person).filter(p->p.getPersonId()>=100).map(Person::getPersonName).orElse("");
+        System.out.println(personName);
+    }
+}
